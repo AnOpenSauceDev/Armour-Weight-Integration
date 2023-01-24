fe2813233124e6ae9bb69f5adb7d4d678f6570c2
AnOpenSauceDev <119765865+AnOpenSauceDev@users.noreply.github.com>
2023-01-24 14:33:26 +1000

    Initial commit

---
+++ src/main/java/an/opensauce/mudmat.java
@@ -0,0 +1,56 @@
+package an.opensauce;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class mudmat implements ArmorMaterial {
+
+    private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
+    private static final int[] PROTECTION_VALUES = new int[] {1, 2, 3, 1};
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()] * 1;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION_VALUES[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 1;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.BLOCK_BEEHIVE_DRIP;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.MUD);
+    }
+
+    @Override
+    public String getName() {
+        // Must be all lowercase
+        return "mud";
+    }
+
+    @Override
+    public float getToughness() {
+        return 1.0F;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0.1F;
+    }
+
+}
---
+++ src/main/java/an/opensauce/weightIntegration.java
@@ -0,0 +1,33 @@
+package an.opensauce;
+
+import an.opensauce.armourweight.Armourweight;
+import an.opensauce.armourweight.api.armourType;
+import an.opensauce.armourweight.api.armourWeightDef;
+import an.opensauce.armourweight.util.WeightUtil;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Material;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class weightIntegration implements ModInitializer {
+
+
+
+
+    public static ArmorMaterial Mudmat = new mudmat();
+    public static Item Mud_Chestplate = new ArmorItem(Mudmat, EquipmentSlot.CHEST, new Item.Settings());
+
+    public static armourWeightDef Mud_Chestplate_Def = new armourWeightDef(armourType.CHEST,1f,Mud_Chestplate);
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registries.ITEM,new Identifier("weight_integration","mud_chestplate"),Mud_Chestplate);
+        WeightUtil.addElement(Mud_Chestplate_Def);
+    }
+}
